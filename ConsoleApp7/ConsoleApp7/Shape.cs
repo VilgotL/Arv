16888827d6a0f6665ef187f36b09c50dd8007a7b
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-08-26 09:18:07 +0200

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Shape
+    {
+        public double width;
+        public double height;
+
+        public Shape (double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Triangle : Shape
+    {
+        public Triangle(): base()
+        {
+            
+        }
+
+        public double Area ()
+        {
+
+        }
+
+        public double Circumferance ()
+        {
+
+        }
+    }
+}
