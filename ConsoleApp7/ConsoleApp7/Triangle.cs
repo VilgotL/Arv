d9a1171b76ed2463540c82ae7fd61bd6676c18d1
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-09-01 10:40:40 +0200

    Update Triangle.cs

--- ConsoleApp7/ConsoleApp7/Triangle.cs
+++ ConsoleApp7/ConsoleApp7/Triangle.cs
@@ -1,26 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+namespace ConsoleApp13
 {
     class Triangle : Shape
     {
-        public Triangle(): base()
-        {
-            
-        }
-
-        public double Area ()
+        public Triangle(double width, double height):base(width, height)
         {
 
         }
 
-        public double Circumferance ()
+        public double Area()
         {
+            double area = width * height;
+            area /= 2;
+            return area;
+        }
 
+        public double Circumferance()
+        {
+            double hypotenusa = Math.Sqrt(width * width + height * height);
+            double circ = width + height + hypotenusa;
+            return circ;
         }
     }
 }
