2b52f8ba832206f06da129cb2427a74a55df9dfe
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-09-08 20:56:13 +0200

    Update Program.cs

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -14,21 +14,21 @@ namespace ConsoleApp13
             double bredd;
             int form;
             Console.WriteLine("Höjd: ");
-            höjd = Convert.ToDouble(Console.Read());
+            höjd = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Bredd: ");
-            bredd = Convert.ToDouble(Console.Read());
+            bredd = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Skriv 1 om det är en rätvinklig triangel och 2 om det är en rektangel");
             form = Convert.ToInt32(Console.Read());
             if (form == 1)
             {
                 Triangle t = new Triangle(bredd, höjd);
-                Console.WriteLine("Area : " + t.Area());
+                Console.WriteLine("Area: " + t.Area());
                 Console.WriteLine("Omkrets: " + t.Circumferance());
             }
             if (form == 2)
             {
                 Rectangle r = new Rectangle(bredd, höjd);
-                Console.WriteLine("Area : " + r.Area());
+                Console.WriteLine("Area: " + r.Area());
                 Console.WriteLine("Omkrets: " + r.Circumferance());
             }
         }
